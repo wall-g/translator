bc3689404ec6d4cc8665e682f48f637e43272764
Gaurav <gswaldia25@gmail.com>
2021-03-25 16:53:22 +0530

    added script

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ var btn = document.querySelector('#btn-translate');
 var textInput = document.querySelector('#textarea');
 var outputDiv = document.querySelector('#output');
 
-btn.addEventListener('click', clickEvent);
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time");
+}
 
 function clickEvent(){
-    outputDiv.innerText = textInput.value;
+    var inputText = textInput.value; // taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+
     
 };
+btn.addEventListener("click", clickEvent);
